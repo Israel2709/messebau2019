f103e5e090dfbc443c8350fa548b79b0d0996655
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-05-17 20:33:26 -0500

    [FEAT] side nav control

--- js/main.js
+++ js/main.js
@@ -1,74 +1,20 @@
-var categoryArray = [
-    "html",
-    "css",
-    "bs",
-    "html",
-    "css",
-    "bs",
-    "kode",
-    "html",
-    "css",
-    "bs",
-    "kode"
-]
+/*$(document).bind('scroll',function(e){
+   console.log(window.pageYOffset);
+   console.log($("#section-1").offset().top);
+   $(".spy-section").each(()=>{
+   	console.log($(this).offset().top);
+   })
+});*/
 
-var difficultyArray = [
-    "Alejandra",
-    "Ann",
-    "Noemi",
-    "Erik",
-    "Jorge",
-    "Carlos",
-    "José",
-    "Héctor",
-    "Agus",
-    "Ochoa",
-    "Rodo",
-    "Lupita",
-    "Diana"
-]
+$(".wrapper-section-active .bullet").click((event)=>{
 
-var questionScore;
-var randomActive = false;
-var selectedName;
-var randomIndex;
+	$(".wrapper-section-active .bullet").removeClass("active");
+	$(event.target).addClass("active");
+	let selectedSection = $(event.target).data("section-target");
+	console.log(selectedSection);
 
-var timer;
+	$('html, body').animate({
+        scrollTop: $("#"+selectedSection).offset().top
+    }, 1000);
 
-function toggleRandom(button) {
-    if ($(button).hasClass("active")) {
-        clearTimeout(timer)
-        $(button).removeClass("active").text("Iniciar");
-        var choosen = difficultyArray[randomIndex]
-        console.log(choosen)
-        $(".difficulty-wrapper").text(choosen)
-        console.log(difficultyArray);
-        console.log(choosen);
-        difficultyArray.splice(randomIndex, 1);
-        console.log(difficultyArray);
-        randomActive = false;
-    } else {
-        $(button).addClass("active").text("Detener");
-        randomActive = true;
-        randomizeNames();
-    }
-}
-
-function addScore(button) {
-    var currentInput = $(button).closest(".team-wrapper").find(".team-score");
-    var currentScore = parseInt(currentInput.val());
-    var updatedScore = currentScore + questionScore;
-    currentInput.val(updatedScore)
-}
-
-function randomizeNames() {
-    if (randomActive == true && $("#randomize").hasClass("active")) {
-        timer = setTimeout(function() {
-            var randomD = Math.floor(Math.random() * (+difficultyArray.length - +0)) + +0;
-            randomIndex = randomD;
-            $(".difficulty-wrapper").text(difficultyArray[randomD]);
-            selectedName = difficultyArray[randomD];
-            randomizeNames()
-        }, 20)
-    }
-}
+})
