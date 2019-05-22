2754fa3c7635c04828fe86695a3db62e28d86c6f
ShalemSolis <ssolis@anzen.com.mx>
2019-05-21 20:32:52 -0500

    Cambios fondo issue

---
+++ firebase-deploy/public/js/main.js
@@ -0,0 +1,18 @@
+$(document).bind('scroll',function(e){
+   let currentScrollPosition = Math.floor((window.pageYOffset + 100) / $(window).height())
+   $(".wrapper-section-active .bullet").removeClass("active");
+   $(".wrapper-section-active .bullet:eq("+currentScrollPosition+")").addClass("active");
+});
+
+$(".wrapper-section-active .bullet").click((event)=>{
+	//$(".wrapper-section-active .bullet").removeClass("active");
+	//$(event.target).addClass("active");
+	let selectedSection = $(event.target).data("section-target");
+	$('html, body').animate({
+        scrollTop: $("#"+selectedSection).offset().top
+    }, 1000);
+})
+
+$(".navbar button, .aside-navbar .close-icon, .nav-link").click(()=>{
+	$(".aside-navbar").toggleClass("hidden-navbar");
+})
