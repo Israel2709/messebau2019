accdccb028e3c8a65fde8fa6a2fbee68082343ae
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-05-21 23:25:42 -0500

    [FEAT] Section animation :octocat:

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ $(document).bind('scroll',function(e){
    let currentScrollPosition = Math.floor((window.pageYOffset + 100) / $(window).height())
    $(".wrapper-section-active .bullet").removeClass("active");
    $(".wrapper-section-active .bullet:eq("+currentScrollPosition+")").addClass("active");
+   $(`#section-${currentScrollPosition +1}, #section-${currentScrollPosition +1} *`).removeClass("left-hidden");
 });
+
+$(document).ready(()=>{
+	$('html, body').animate({
+        top: 0
+    }, 500,()=>{
+    	$("#section-1, #section-1 *").removeClass("left-hidden");
+    });
+	
+})
 
 $(".wrapper-section-active .bullet").click((event)=>{
 	//$(".wrapper-section-active .bullet").removeClass("active");
@@ -10,7 +20,9 @@ $(".wrapper-section-active .bullet").click((event)=>{
 	let selectedSection = $(event.target).data("section-target");
 	$('html, body').animate({
         scrollTop: $("#"+selectedSection).offset().top
-    }, 1000);
+    }, 500,()=>{
+    	$("#"+selectedSection+", #"+selectedSection+" *").removeClass("left-hidden");
+    });
 })
 
 $(".navbar button, .aside-navbar .close-icon, .nav-link").click(()=>{
